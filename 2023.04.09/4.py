3f8f26210de4394cd377b7606de3d926a34709f9
Gennadiy <renardes@gmail.com>
2023-04-12 19:54:58 +0500

    2023.04.09: комментарии, правки, итоги

--- 2023.04.09/4.py
+++ 2023.04.09/4.py
@@ -1,5 +1,6 @@
 num_input = int(input("Введите трехзначное число: "))
 
+# КОММЕНТАРИЙ: цифра — digit, число — number, num
 num = num_input // 10
 num_one = num // 10
 num_two = num % 10
@@ -7,13 +8,14 @@ num_three = num_input % 10
 
 # Сумма цифр
 sum_num = num_one + num_two + num_three
-
 # Произведение цифр
 multi_num = num_one * num_two * num_three
-
 
 print(f"Сумма цифр = {sum_num} \nПроизведение цифр = {multi_num}")
 
 # Введите трехзначное число: 555
 # Сумма цифр = 15
 # Произведение цифр = 125
+
+
+# ИТОГ: отлично — 4/4
