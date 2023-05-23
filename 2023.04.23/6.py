c04b08a48acecee3e4a2b9f2f68a85aebf7d5dfa
Gennadiy <renardes@gmail.com>
2023-05-23 18:10:05 +0500

    2023.04.23: комментарии, правки, итоги

--- 2023.04.23/6.py
+++ 2023.04.23/6.py
@@ -1,11 +1,15 @@
-ticket = [int(i) for i in input("Введите номер билета: ")]
+ticket = [int(char) for char in input("Введите номер билета: ")]
 if sum(ticket[:3]) == sum(ticket[3:]):
     print('Да')
 else:
     print('Нет')
-    
+
+
 # Введите номер билета: 645708
 # Да
 
 # Введите номер билета: 956234
 # Нет
+
+
+# ИТОГ: отлично — 3/3
