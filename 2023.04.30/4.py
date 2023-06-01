ad3e3000f88f7ddd8e6ab7c01a84f8b23b2ff93f
Gennadiy <renardes@gmail.com>
2023-06-01 18:31:23 +0500

    2023.04.30: комментарии, правки, итоги

--- 2023.04.30/4.py
+++ 2023.04.30/4.py
@@ -4,6 +4,7 @@ while True:
     inp = input('Введите число через пробел: ')
     if not inp:
         break
+    # УДАЛИТЬ: избыточная переменная — к пустой строке тоже можно применить метод split() — он вернёт пустой список
     list_inp = inp.split()
     case[list_inp[0]] = list_inp[1]
 
@@ -12,6 +13,7 @@ for k, v in case.items():
     if v == inp_value:
         print(k)
         break
+# КОММЕНТАРИЙ: вот это хорошо!
 else:
     print('! value error !')
 
@@ -33,3 +35,4 @@ else:
 # ! value error !
 
 
+# ИТОГ: очень хорошо — 3/4
--- 2023.04.30/8.py
+++ 2023.04.30/8.py
@@ -1,16 +1,17 @@
+# УДАЛИТЬ: переменная inp избыточна
 inp = input('Введите имена файлов: ')
-
 list_inp = sorted(inp.split('; '))
 
 list_cout = []
-
 dict_copy = {}
 
 for i in list_inp:
     dict_copy[i] = dict_copy.get(i, 0) + 1
+    # КОММЕНТАРИЙ: хорошо, что в один цикл пошли
     if dict_copy[i] == 1:
         list_cout.append(i)
     else:
+        # ИСПРАВИТЬ: метод index() вызывается лишний раз — оптимизируйте
         list_cout.append(i[:i.index('.')] + '_' + str(dict_copy[i]) + i[i.index('.'):])
 
 print(*list_cout, sep='\n')
@@ -29,3 +30,4 @@ print(*list_cout, sep='\n')
 # src_2.tar.gz
 
 
+# ИТОГ: хорошо, требуется немного доработать — 4/6
